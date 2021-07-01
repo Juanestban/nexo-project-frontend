a842944a5baea24e012156ba5fb848ac0d72208c
Juan esteban <juanestban000@gmail.com>
2021-07-01 08:05:12 -0500

    add new modules

--- src/app/imports/angular-material.ts
+++ src/app/imports/angular-material.ts
@@ -4,6 +4,9 @@ import { MatRippleModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { PortalModule } from '@angular/cdk/portal';
 
 const AngularMaterialModule: any = [
   MatTableModule,
@@ -12,6 +15,9 @@ const AngularMaterialModule: any = [
   MatPaginatorModule,
   MatSelectModule,
   MatToolbarModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  PortalModule,
 ];
 
 export default AngularMaterialModule;
