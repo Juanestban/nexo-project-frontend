a0f9bdd46223b814d9c000cc9b0cda089a93b39c
Juan Esteban <juanestban000@gmail.com>
2021-06-30 22:53:06 -0500

    change css and html of app.module.html || delete the public variable from app.component.ts || update the imports for use all components created to project nexo-frontend angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MerchandisesPageComponent } from './pages/merchandises-page/merchandises-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { AddOrEditMerchPageComponent } from './pages/add-or-edit-merch-page/add-or-edit-merch-page.component';
+import { TableMerchandisesComponent } from './components/Merchandises/table-merchandises/table-merchandises.component';
+import { FormMerchandiseComponent } from './components/Merchandises/form-merchandise/form-merchandise.component';
+import { TableUsersComponent } from './components/Users/table-users/table-users.component';
+import { FormUserComponent } from './components/Users/form-user/form-user.component';
+import { ModalChargeComponent } from './components/Charges/modal-charge/modal-charge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MerchandisesPageComponent,
+    UsersPageComponent,
+    AddOrEditMerchPageComponent,
+    TableMerchandisesComponent,
+    FormMerchandiseComponent,
+    TableUsersComponent,
+    FormUserComponent,
+    ModalChargeComponent
   ],
   imports: [
     BrowserModule,
